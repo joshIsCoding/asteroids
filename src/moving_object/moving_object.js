f26ff8edcd624fbf7733b482d87b4b079144e588
Josh Brown <yes_you_do@hotmail.com>
2022-09-18 17:03:02 +0100

    Add MovingObject.prototype.draw()

--- src/moving_object/moving_object.js
+++ src/moving_object/moving_object.js
@@ -1,8 +1,17 @@
 function MovingObject(opts) {
-  this.pos = opts.pos;
+  this.xPos = opts.pos[0];
+  this.yPos = opts.pos[1];
   this.vel = opts.vel;
   this.radius = opts.radius;
   this.color = opts.color;
+}
+
+MovingObject.prototype.draw = function(ctx) {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.xPos, this.yPos, this.radius, 0, 360);
+  ctx.closePath();
+  ctx.fill();
 }
 
 module.exports = MovingObject;
