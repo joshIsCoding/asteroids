fa2ebab5b9acdb9959232beed8d6c3f2b19904b0
Josh Brown <yes_you_do@hotmail.com>
2022-09-18 17:02:41 +0100

    Fetch the canvas element and store it

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const MovingObject = require("./moving_object/moving_object");
 
 window.MovingObject = MovingObject;
 
+const canvas = document.getElementById('game-canvas');
+
 console.log('Webpack is working!');
